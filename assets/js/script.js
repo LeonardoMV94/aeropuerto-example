d328e3c58dcac065b35395270a578e607db2dd1d
Simone R. Lubones <simone.rlubones@gmail.com>
2024-08-28 03:00:05 -0400

    creación de contacto

--- assets/js/script.js
+++ assets/js/script.js
@@ -125,3 +125,36 @@ const randomIntFromInterval = (min, max) => {
 setInterval(() => {
   cambiarBodyTabla(vuelos);
 }, 3000);
+
+
+// Nueva funcionalidad para el formulario y el modal
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+  event.preventDefault(); // Evita el envío del formulario y el refresco de la página
+
+  // Mostrar el modal
+  let modal = new bootstrap.Modal(document.getElementById('exampleModal'));
+  modal.show();
+
+  // Vaciar el formulario después de que el modal sea mostrado
+  this.reset();
+});
+
+
+// Nueva funcionalidad para el radio
+document.addEventListener('DOMContentLoaded', (event) => {
+  const radioButtons = document.querySelectorAll('input[type="radio"]');
+  
+  radioButtons.forEach(radio => {
+      radio.addEventListener('change', function() {
+          // Si el radio button ya está seleccionado, desmarcarlo
+          if (this.checked) {
+              // Desmarcar el radio button después de un pequeño retraso para permitir que el estado se actualice
+              setTimeout(() => {
+                  this.checked = false;
+              }, 0);
+          }
+      });
+  });
+});
+
+
