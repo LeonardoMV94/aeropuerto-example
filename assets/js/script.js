2a23f46ba0f16f19bc6000cd4bd8c10cd139d059
Simone R. Lubones <simone.rlubones@gmail.com>
2024-08-28 20:46:20 -0400

    corrección estilos y buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -172,3 +172,13 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 });
 
+
+// Nueva funcionalidad para el boton footer
+$(document).ready(function() {
+  $('a.arriba').on('click', function(e) {
+      e.preventDefault(); // Evitar el comportamiento predeterminado del enlace
+      $('html, body').animate({
+          scrollTop: $($(this).attr('href')).offset().top
+      }, 800); // 800 milisegundos para un desplazamiento suave
+  });
+});
